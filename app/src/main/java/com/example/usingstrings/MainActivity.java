1dff8381f88f3df513a20912d3f101ac874262cd
sabesan1998 <sabesanganeshalingam@gmail.com>
2020-08-08 14:07:37 +0530

    Tutorial 2

---
+++ app/src/main/java/com/example/usingstrings/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.usingstrings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView txtView2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.i("Lifecycle","onCreate() invoked" );
+
+
+        txtView2= findViewById(R.id.textView2);
+        txtView2.setText(R.string.Msg2);
+
+    }
+    public void onStart()
+    {
+     super.onStart();
+     Log.i("Lifecycle", "onStart() invoked");
+
+    }
+
+    public void onRestart(){
+        super.onRestart();
+        Log.i("Lifecycle", "onRestart() invoked");
+
+    }
+    public void onResume(){
+        super.onResume();
+        Log.i("Lifecycle", "onResume() invoked");
+    }
+
+
+}
